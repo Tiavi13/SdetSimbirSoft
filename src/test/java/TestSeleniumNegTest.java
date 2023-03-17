123d78ff59922caf9f2a430ed0e5376522cdaa37
Tiavi13 <toshchevrv@gmail.com>
2023-03-17 17:43:13 +0300

    Использовал паттерн проектирования Page Object.

--- src/test/java/TestSeleniumNegTest.java
+++ src/test/java/TestSeleniumNegTest.java
@@ -1,18 +1,31 @@
-import org.openqa.selenium.By;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class TestSeleniumNegTest {
+    public static LoginPage loginPage;
+    public static WebDriver driver;
 
-class TestSeleniumNegTest {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+
+    @BeforeClass
+    public static void setup() {
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("test");
-        driver.findElement(By.id("password")).sendKeys("test");
-        driver.findElement(By.id("login-button")).click();
-        assertTrue(driver.getPageSource().contains("Epic sadface: Username and password do not match any user in this service"));
-        driver.close();
-
     }
+    @Test
+    public void loginTest() {
+        loginPage.inputLogin("test");
+        loginPage.inputPasswd("test");
+        loginPage.clickLoginBtn();
+        loginPage.assertText("Epic sadface: Username and password do not match any user in this service");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit(); }
 }
--- src/test/java/TestSeleniumTest.java
+++ src/test/java/TestSeleniumTest.java
@@ -1,31 +1,50 @@
-import dev.failsafe.internal.util.Assert;
-import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.By;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class TestSeleniumTest {
+    public static LoginPage loginPage;
+    public static CatalogPage catalogPage;
+    public static CartPage cartPage;
+    public static CheckoutPage checkoutPage;
+    public static OverviewPage overviewPage;
+    public static SuccessPage successPage;
+    public static WebDriver driver;
 
-class TestSeleniumTest {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+
+    @BeforeClass
+    public static void setup() {
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        catalogPage = new CatalogPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutPage = new CheckoutPage(driver);
+        overviewPage = new OverviewPage(driver);
+        successPage = new SuccessPage(driver);
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.cssSelector("input#user-name")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("input#password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-        driver.findElement(By.name("add-to-cart-sauce-labs-backpack")).click();
-        driver.findElement(By.id("shopping_cart_container")).click();
-        driver.findElement(By.id("checkout")).click();
-        driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("test");
-        driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys("test");
-        driver.findElement(By.xpath("//input[@name='postalCode']")).sendKeys("test");
-        driver.findElement(By.name("continue")).click();
-        driver.findElement(By.id("finish")).click();
-        String actualUrl = driver.getCurrentUrl();
-        String expectedUrl = "https://www.saucedemo.com/checkout-complete.html";
-        assertEquals(expectedUrl, actualUrl);
-        assertTrue(driver.getPageSource().contains("Thank you for your order!"));
-        driver.close();
     }
+    @Test
+    public void loginTest() {
+        loginPage.inputLogin("standard_user");
+        loginPage.inputPasswd("secret_sauce");
+        loginPage.clickLoginBtn();
+        catalogPage.clickAddToCartButton();
+        catalogPage.clickCartButton();
+        cartPage.clickCheckoutButton();
+        checkoutPage.firstNameInput("test");
+        checkoutPage.lastNameInput("test");
+        checkoutPage.postalCodeInput("test");
+        checkoutPage.clickContinueButton();
+        overviewPage.clickFinishButton();
+        successPage.assertText("Thank you for your order!");
+        successPage.assertUrl("https://www.saucedemo.com/checkout-complete.html");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit(); }
 }
